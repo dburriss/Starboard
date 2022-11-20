48fcc332a6971063edb8fa817cd22947b0044c98
dburriss <dburriss@users.noreply.github.com>
2022-11-20 20:52:12 +0000

    Deploying to gh-pages from @ dburriss/overboard@ee3bb901c7bc2645c4f3ec734e5378ef4d87dc6d 🚀

--- tutorials/cleanup-guestbook.ps1
+++ tutorials/cleanup-guestbook.ps1
@@ -1,20 +1,8 @@
 kubectl delete service guestbook
 kubectl delete deployment guestbook
 
-kubectl delete service redis-replica
-kubectl delete deployment redis-replica
-
-kubectl delete service redis-slave
-kubectl delete deployment redis-slave
-
 kubectl delete service redis-follower
 kubectl delete deployment redis-follower
 
-kubectl delete service redis-master
-kubectl delete deployment redis-master
-
 kubectl delete service redis-leader
 kubectl delete deployment redis-leader
-
-kubectl delete service frontend
-kubectl delete deployment frontend
